f86f527b66f97c876b344e14e7f234bb6f5a6ba4
harshbutfair <harsh.narayan@yahoo.com>
2021-06-23 21:16:08 +0530

    Added remove list

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ loadEventListeners();
 function loadEventListeners() {
     // Add task event
     form.addEventListener('submit', addTask);
+    // Remove task event
+    taskList.addEventListener('click' , removeTask);
 }
 
 // Add Task
@@ -44,4 +46,15 @@ function addTask(e){
 
 
     e.preventDefault();
+}
+
+// Remove Task
+
+function removeTask(e) {
+    if(e.target.parentElement.classList.contains('delete-item'))
+        {
+            if(confirm('Are you sure?')) {
+            e.target.parentElement.parentElement.remove();
+            }
+    }
 }
